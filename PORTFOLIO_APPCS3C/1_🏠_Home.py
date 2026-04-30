f25bb0a7aca06e7bc091be590e7088a2a43b7e9a
belenfajardo <belenfajardo321@gmail.com>
2026-04-30 12:30:36 +1030

    Update 1_🏠_Home.py

--- PORTFOLIO_APPCS3C/1_🏠_Home.py
+++ PORTFOLIO_APPCS3C/1_🏠_Home.py
@@ -90,5 +90,5 @@ with col1:
 
 with col2:
     st.markdown('<div class="img-glow">', unsafe_allow_html=True)
-    st.image("Fajardo Belen.png", width=250)
+    st.image("", width=250)
     st.markdown('</div>', unsafe_allow_html=True)
